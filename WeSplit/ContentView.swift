3c84d72f14892c51046f6ba4b8cb53da1e865f58
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2023-10-02 10:20:41 +0300

    Adds form view with NavigationView and title

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section{
+                    Text("Hello, world!")
+                }
+                
+                Section{
+                    Text("Hello, world!")
+                    Text("Hello, world!")
+                }
+            }
+            .navigationTitle("SwiftUI")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
