490b4c849bc2a1d94803eaa75ae4857965f8180d
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2023-10-02 11:59:59 +0300

    calculates amount per person

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,22 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let students = ["Harry", "Harry", "Harry"]
-    
     @State
-    private var selectedStudent = "Harry"
+    private var checkAmount = 0.0
+
+    @State
+    private var numberOfPeople = 2
+
+    @State
+    private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
     
     var body: some View {
         NavigationView {
             Form {
-                Picker("Select your student", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "KSH"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
                     }
                 }
+                
+                Section {
+                    
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "KSH"))
+                }
             }
+            .navigationTitle("WeSplit")
         }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
